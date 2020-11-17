051c69f24cb051a78f5ea8b2b3f68a91d8c2f451
ezrc2 <ezchen2556@gmail.com>
2020-11-16 20:27:50 -0800

    Added code in constructor to calculate costs of every cell

--- include/core/maze.h
+++ include/core/maze.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <vector>
+
+#include "cell.h"
+#include <glm/glm.hpp>
+
 class Maze {
  public:
-  Maze();
+  Maze(const std::vector<std::vector<size_t>>& maze_cells,
+       const glm::vec2& start, const glm::vec2& end);
+
+  void UpdateSearchLoop();
+
+  std::vector<Cell> GetNeighbors(const Cell& cell);
+
+  std::vector<Cell> GetClosedCells() const;
+
+ private:
+
+  std::vector<Cell> open_cells_;
+  std::vector<Cell> closed_cells_;
+
+  glm::vec2 current_location_;
 };
