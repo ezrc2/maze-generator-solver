1a5b7fb3af61dcc2e254e4380600e2348150cc69
ezrc2 <ezchen2556@gmail.com>
2020-11-17 21:45:16 -0800

    Added pointer to previous cell

--- include/core/cell.h
+++ include/core/cell.h
@@ -2,13 +2,13 @@
 
 #include <vector>
 #include <glm/vec2.hpp>
-#include "point.h"
 
 struct Cell {
 
   glm::vec2 location;
   float g_cost; // Distance from start cell
   float h_cost; // Distance from end cell
-  float f_cost = g_cost + h_cost;
+  float f_cost;
+  Cell* previous;
 
 };
