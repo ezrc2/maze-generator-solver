965efef31ed976724ed5911b046a9673bd3579a4
ezrc2 <ezchen2556@gmail.com>
2020-11-29 23:12:19 -0800

    Added test for correct h costs of solution path

--- tests/maze_test.cc
+++ tests/maze_test.cc
@@ -33,6 +33,24 @@ TEST_CASE("Solution G costs") {
   }
 }
 
+TEST_CASE("Solution H costs") {
+  std::vector<float> h_costs =
+      {7.2111f, 6.4031f, 5.831f, 5.3852f, 4.4721f, 3.60555f, 4.2426f, 5.0f,
+       4.4721f, 4.1231f, 3.1623f, 2.23607f, 1.4142f, 1.0f, 0.0f};
+
+  MazeSolver solver(kMazeCells, kStartCell, kEndCell);
+  while (!solver.IsMazeSolved()) {
+    solver.UpdateSearchLoop();
+  }
+
+  std::vector<Cell*> solution = solver.GetSolutionPath();
+  REQUIRE(solution.size() == h_costs.size());
+
+  for (size_t i = 0; i < solution.size(); i++) {
+    REQUIRE(h_costs[i] == Approx(solution[i]->h_cost));
+  }
+}
+
 TEST_CASE("Solution F costs") {
   std::vector<float> f_costs =
       {7.2111f, 7.4031f, 7.2452f, 7.6212f, 7.3006f, 7.2111f, 7.4049f, 8.0f,
