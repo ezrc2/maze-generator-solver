8f702baf1b367700678bd2ae234d04a608902fad
ezrc2 <ezchen2556@gmail.com>
2020-12-08 11:48:06 -0800

    Removed unused header file and added comment

--- include/core/cell.h
+++ include/core/cell.h
@@ -1,17 +1,16 @@
 #pragma once
 
-#include <vector>
 #include <glm/vec2.hpp>
 
 /**
- * Represents a cell on the maze
+ * Represents a cell in the maze
  */
 struct Cell {
 
   glm::vec2 location;
   float g_cost; // Distance from start cell
   float h_cost; // Distance from end cell
-  float f_cost;
+  float f_cost; // g cost + h cost
   Cell* previous = nullptr;
 
 };
