58266e7f952737e308976c537e9c2bc830bfd18b
ezrc2 <ezchen2556@gmail.com>
2020-12-08 16:02:07 -0800

    Added documentation and constants for path and wall

--- include/core/maze_generator.h
+++ include/core/maze_generator.h
@@ -4,21 +4,50 @@
 #include <random>
 #include <algorithm>
 
+/**
+ * Generates a maze using randomized depth-first search
+ */
 class MazeGenerator {
-
  public:
 
+  /**
+   * Initializes the 2D vector of cells
+   *
+   * @param height The height of the maze
+   * @param width The width of the maze
+   */
   MazeGenerator(int height, int width);
 
+  /**
+   * Chooses a random starting point and generates the maze
+   */
   void GenerateMaze();
 
+  /**
+   * @return The 2D vector representing the maze
+   */
   std::vector<std::vector<int>> GetMazeCells();
 
  private:
 
+  /**
+   * Randomized depth-first search
+   *
+   * @param row The row of the cell
+   * @param col The column of the cell
+   */
   void RandomDFS(int row, int col);
 
+  /**
+   * Creates a vector of length 4 that represent random directions
+   * 0: up, 1: down, 2: left, 3: right
+   *
+   * @return The vector of random directions
+   */
   std::vector<int> GetRandomDirections();
+
+  const int kPath = 1;
+  const int kWall = 0;
 
   std::vector<std::vector<int>> cells_;
   int height_;
--- src/core/maze_generator.cc
+++ src/core/maze_generator.cc
@@ -4,7 +4,7 @@ MazeGenerator::MazeGenerator(int height, int width) {
   height_ = height;
   width_ = width;
   for (int i = 0; i < height; i++) {
-    std::vector<int> row(width, 0);
+    std::vector<int> row(width, kWall);
     cells_.push_back(row);
   }
 }
@@ -19,7 +19,7 @@ void MazeGenerator::GenerateMaze() {
     col = rand() % width_;
   }
 
-  cells_[row][col] = 1;
+  cells_[row][col] = kPath;
   RandomDFS(row, col);
 }
 
@@ -27,37 +27,38 @@ void MazeGenerator::RandomDFS(int row, int col) {
   std::vector<int> directions = GetRandomDirections();
   for (int direction : directions) {
     switch (direction) {
-      case 0: // up
-        if (row - 2 > 0 && cells_[row - 2][col] == 0) {
-          cells_[row - 2][col] = 1;
-          cells_[row - 1][col] = 1;
+      case 0: // Up
+        if (row - 2 > 0 && cells_[row - 2][col] == kWall) {
+          cells_[row - 2][col] = kPath;
+          cells_[row - 1][col] = kPath;
           RandomDFS(row - 2, col);
         }
         break;
-      case 1: // down
-        if (row + 2 < height_ - 1 && cells_[row + 2][col] == 0) {
-          cells_[row + 2][col] = 1;
-          cells_[row + 1][col] = 1;
+      case 1: // Down
+        if (row + 2 < height_ - 1 && cells_[row + 2][col] == kWall) {
+          cells_[row + 2][col] = kPath;
+          cells_[row + 1][col] = kPath;
           RandomDFS(row + 2, col);
         }
         break;
-      case 2: // left
-        if (col - 2 > 0 && cells_[row][col - 2] == 0) {
-          cells_[row][col - 2] = 1;
-          cells_[row][col - 1] = 1;
+      case 2: // Left
+        if (col - 2 > 0 && cells_[row][col - 2] == kWall) {
+          cells_[row][col - 2] = kPath;
+          cells_[row][col - 1] = kPath;
           RandomDFS(row, col - 2);
         }
         break;
-      case 3: // right
-        if (col + 2 < width_ - 1 && cells_[row][col + 2] == 0) {
-          cells_[row][col + 2] = 1;
-          cells_[row][col + 1] = 1;
+      case 3: // Right
+        if (col + 2 < width_ - 1 && cells_[row][col + 2] == kWall) {
+          cells_[row][col + 2] = kPath;
+          cells_[row][col + 1] = kPath;
           RandomDFS(row, col + 2);
         }
         break;
     }
   }
-  cells_[height_ - 2][width_ - 1] = 1;
+  // Exit is bottom right corner
+  cells_[height_ - 2][width_ - 1] = kPath;
 }
 
 std::vector<int> MazeGenerator::GetRandomDirections() {
